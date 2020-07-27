db9b6331b166b3c9d5be6e90ae20193358c03c7a
Phongpak Taothong <phongpak@bsd.education>
2020-07-27 15:43:49 +0700

    Make lhci failed 2

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-      preset: 'lighthouse:recommended',
+      preset: 'lighthouse:no-pwa',
       "assertions": {
         "categories:performance": ["warn", { "minScore": 0.9 }],
         "categories:accessibility": ["error", { "minScore": 1 }]
